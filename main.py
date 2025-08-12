058e1a82bdfb2faaffcc6db7fda72922be420760
Sahil Meena <sahilmeena3435@gmail.com>
2025-08-12 14:39:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 ACCESS_TOKEN = "EAA6ijAIaH6MBPDGRiR4Ju3KG5O1YIj2Wb0sRPk0BLvE57NnQhe6y07x8MxJN35tuNuBgMwf5E9dHOJHZALXZBeTMLStAXNowcteTubU0mXNDfOL0hOC2NZBfzCt8rqmVUT7OV9qK5ZBRP8hbt2mJcS2APZBekWP2k0Vqeq522bI9QwdcchKvv1M15HBD9zuraFViZBoWyV8OEofdoan3kw90Hqb2nfhOsZCH4nM6CcwGHttK4QZD"
 PHONE_NUMBER_ID = "764448353413111"
-VERIFY_TOKEN = "testtoken"  # Use the same token you entered in Meta verification
+VERIFY_TOKEN = "testwebhook"  # Use the same token you entered in Meta verification
 
 def send_message(to, text):
     url = f"https://graph.facebook.com/v17.0/{PHONE_NUMBER_ID}/messages"
